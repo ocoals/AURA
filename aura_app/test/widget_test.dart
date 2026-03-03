4f707e841c747786c2b812ff57947f9341b71f7a
ocoals <ohchaemin00@gmail.com>
2026-03-03 19:16:11 +0900

    Part2.1 수정

--- aura_app/test/widget_test.dart
+++ aura_app/test/widget_test.dart
@@ -6,8 +6,13 @@ import 'package:aura_app/app/app.dart';
 void main() {
   testWidgets('App renders without crashing', (WidgetTester tester) async {
     await tester.pumpWidget(const ProviderScope(child: AuraApp()));
-    await tester.pumpAndSettle();
+    await tester.pump();
 
-    expect(find.text('Aura'), findsOneWidget);
+    // Initial route is /splash which shows 'AURA' logo
+    expect(find.text('AURA'), findsOneWidget);
+
+    // Advance past the splash timer (1s) to avoid pending timer error
+    await tester.pump(const Duration(seconds: 1));
+    await tester.pumpAndSettle();
   });
 }
